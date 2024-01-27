37e3beca15cd66bee6837dbad7c6df7191a16ffc
IvanKaragyozov <ivankaragyozov0@gmail.com>
2024-01-27 16:11:35 +0200

    TMS: Created first endpoint

--- tms-api/src/main/java/pu/master/tmsapi/services/UserService.java
+++ tms-api/src/main/java/pu/master/tmsapi/services/UserService.java
@@ -1,9 +1,15 @@
 package pu.master.tmsapi.services;
 
 
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import pu.master.tmsapi.models.dtos.UserDto;
+import pu.master.tmsapi.models.entities.User;
+import pu.master.tmsapi.models.requests.UserRequest;
 import pu.master.tmsapi.repositories.UserRepository;
 
 
@@ -13,10 +19,35 @@ public class UserService
 
     private final UserRepository userRepository;
 
+    private final ModelMapper modelMapper;
+
 
     @Autowired
-    public UserService(final UserRepository userRepository)
+    public UserService(final UserRepository userRepository, final ModelMapper modelMapper)
     {
         this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
+    }
+
+
+    public User createUser(final UserRequest userRequest)
+    {
+        final User user = convertUserRequestToUser(userRequest);
+        return this.userRepository.save(user);
+    }
+
+
+    public List<UserDto> getAllUsers()
+    {
+        final List<User> allUsers = this.userRepository.findAll();
+
+        return allUsers.stream()
+                        .map(user -> modelMapper.map(user, UserDto.class)).toList();
+    }
+
+
+    private User convertUserRequestToUser(final UserRequest userRequest)
+    {
+        return this.modelMapper.map(userRequest, User.class);
     }
 }
