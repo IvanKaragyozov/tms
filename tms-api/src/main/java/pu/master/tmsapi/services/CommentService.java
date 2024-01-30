6e72a9b731278127185b16110bb0316d8d296221
IvanKaragyozov <ivankaragyozov0@gmail.com>
2024-01-31 00:04:50 +0200

    TMS: Fixed issue when creating a Comment didn't set User and Task

--- tms-api/src/main/java/pu/master/tmsapi/services/CommentService.java
+++ tms-api/src/main/java/pu/master/tmsapi/services/CommentService.java
@@ -6,11 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import pu.master.tmsapi.models.dtos.CommentDto;
-import pu.master.tmsapi.models.dtos.UserDto;
 import pu.master.tmsapi.models.entities.Comment;
+import pu.master.tmsapi.models.entities.Task;
 import pu.master.tmsapi.models.entities.User;
 import pu.master.tmsapi.models.requests.CommentRequest;
-import pu.master.tmsapi.models.requests.UserRequest;
 import pu.master.tmsapi.repositories.CommentRepository;
 
 
@@ -20,21 +19,33 @@ public class CommentService
 
     private final CommentRepository commentRepository;
 
+    private final UserService userService;
+    private final TaskService taskService;
+
     private final ModelMapper modelMapper;
 
     @Autowired
-    public CommentService(final CommentRepository commentRepository, final ModelMapper modelMapper)
+    public CommentService(final CommentRepository commentRepository,
+                          final UserService userService,
+                          final TaskService taskService,
+                          final ModelMapper modelMapper)
     {
         this.commentRepository = commentRepository;
+        this.userService = userService;
+        this.taskService = taskService;
         this.modelMapper = modelMapper;
     }
 
     public Comment createComment(final CommentRequest commentRequest)
     {
         final Comment comment = mapCommentRequestToComment(commentRequest);
-        // TODO: Set task and user
+        final User author = this.userService.getUserById(commentRequest.getAuthor());
+        final Task task = this.taskService.getTaskById(commentRequest.getTask());
 
-        return comment;
+        comment.setAuthor(author);
+        comment.setTask(task);
+
+        return this.commentRepository.save(comment);
     }
 
 
