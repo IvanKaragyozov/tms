0efd352701b73084ce5156126ad48e4be210a6a9
IvanKaragyozov <ivankaragyozov0@gmail.com>
2024-01-31 22:29:07 +0200

    TMS: Separated tms-api into tms-api and tms-core modules (NEEDS WORK)

---
+++ tms-core/src/test/java/pu/master/tmscore/TmsCoreApplicationTests.java
@@ -0,0 +1,17 @@
+package pu.master.tmscore;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class TmsCoreApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
