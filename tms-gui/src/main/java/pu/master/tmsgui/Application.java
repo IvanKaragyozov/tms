ac9c3d375627ac320444bb846003c315ef389db5
IvanKaragyozov <ivankaragyozov0@gmail.com>
2024-02-02 22:47:18 +0200

    TMS-GUI: Initial commit for TMS with Vaadin GUI

---
+++ tms-gui/src/main/java/pu/master/tmsgui/Application.java
@@ -0,0 +1,54 @@
+package pu.master.tmsgui;
+
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.sql.init.SqlDataSourceScriptDatabaseInitializer;
+import org.springframework.boot.autoconfigure.sql.init.SqlInitializationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+
+import pu.master.tmsgui.data.SamplePersonRepository;
+
+
+/**
+ * The entry point of the Spring Boot application.
+ * <p>
+ * Use the @PWA annotation make the application installable on phones, tablets and some desktop browsers.
+ */
+@SpringBootApplication
+@Theme(value = "tms-gui", variant = Lumo.DARK)
+public class Application implements AppShellConfigurator
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+    @Bean
+    SqlDataSourceScriptDatabaseInitializer dataSourceScriptDatabaseInitializer(DataSource dataSource,
+                                                                               SqlInitializationProperties properties,
+                                                                               SamplePersonRepository repository)
+    {
+        // This bean ensures the database is only initialized when empty
+        return new SqlDataSourceScriptDatabaseInitializer(dataSource, properties)
+        {
+            @Override
+            public boolean initializeDatabase()
+            {
+                if (repository.count() == 0L)
+                {
+                    return super.initializeDatabase();
+                }
+                return false;
+            }
+        };
+    }
+}
