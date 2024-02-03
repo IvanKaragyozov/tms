7f033808d7a0e33cf5448849722d586e1b604463
IvanKaragyozov <ivankaragyozov0@gmail.com>
2024-02-03 14:04:15 +0200

    TMS-GUI: created method for retrieving user by usernate in UserRepository

--- tms-gui/src/main/java/pu/master/tmsgui/repositories/UserRepository.java
+++ tms-gui/src/main/java/pu/master/tmsgui/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package pu.master.tmsgui.repositories;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,6 @@ import pu.master.tmsgui.models.entities.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>
 {
+
+    Optional<User> findByUsername(String username);
 }
